a1b148605e4391df9f2c789c0e08a893ce92941b
AtlasFc <arjs2225i@gmail.com>
2025-05-28 10:38:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var reply = prospt("Bienvenid@ ¿cuál es tu nombre?", "")
+  var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "")
   alert ( "Es un gusto tenerte aqui " + reply + "!")
 
   let miBoton = document.querySelector('Con el código: JT05 obtendrás un 5% en tu proxima compra');
