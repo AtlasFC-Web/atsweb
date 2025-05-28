188f634c6cae06db27ab3704b2183b070c68a2e7
AtlasFc <arjs2225i@gmail.com>
2025-05-28 10:37:02 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  var reply = prospt("Bienvenid@ ¿cuál es tu nombre?", "")
+  alert ( "Es un gusto tenerte aqui " + reply + "!")
+
+  let miBoton = document.querySelector('Con el código: JT05 obtendrás un 5% en tu proxima compra');
+}
